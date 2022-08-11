38ef1f9c5ab18613dd8b99defbc14ba9dccf1267
Ahmad Hanafi <ahanafi.id@gmail.com>
2022-08-11 08:19:48 +0700

    Refactor(user): Convert plaintext password to hashed password.

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        User::saving(function($user) {
+            $user->password = bcrypt($user->password);
+        });
+    }
 }
